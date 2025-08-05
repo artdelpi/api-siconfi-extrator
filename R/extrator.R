3fa4c3bc6a5046afb1b6c2686c32dd20249dafab
artdelpi <artdelpi@gmail.com>
2025-08-05 14:22:21 -0300

    chore: estrutura inicial do extrator

--- R/extrator.R
+++ R/extrator.R
@@ -1,1 +1,51 @@
-# Implementar...
+library(httr)
+library(jsonlite)
+library(dplyr)
+
+#------------------------------------------------------------
+#        Função principal de extração da API Siconfi               
+#------------------------------------------------------------
+
+extrair_dados_siconfi_anexos_relatorios <- function(){
+
+}
+
+
+extrair_dados_siconfi_dca <- function(){
+
+}
+
+
+extrair_dados_siconfi_entes <- function(){
+
+}
+
+
+extrair_dados_siconfi_extrato_entregas <- function(){
+
+}
+
+
+extrair_dados_siconfi_msc_controle <- function(){
+
+}
+
+
+extrair_dados_siconfi_msc_orcamentaria <- function(){
+
+}
+
+
+extrair_dados_siconfi_msc_patrimonial <- function(){
+
+}
+
+
+extrair_dados_siconfi_rgf <- function(){
+
+}
+
+
+extrair_dados_siconfi_rreo <- function(){
+    
+}
