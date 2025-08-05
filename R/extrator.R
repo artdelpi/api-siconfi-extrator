69951090712052222083b0b5b563d27d5cf8339c
artdelpi <artdelpi@gmail.com>
2025-08-05 17:52:45 -0300

    feat: consulta ao endpoint 'anexos-relatorios'

--- R/extrator.R
+++ R/extrator.R
@@ -7,7 +7,29 @@ library(dplyr)
 #------------------------------------------------------------
 
 extrair_dados_siconfi_anexos_relatorios <- function(){
+    url_base <- "https://apidatalake.tesouro.gov.br/ords/siconfi/tt/anexos-relatorios"
 
+    # Requisição HTTPS
+    resposta <- tryCatch(
+        GET(url=url_base),
+        error = function(e) {
+            warning("Erro na requisição: ", conditionMessage(e))
+            return(NULL)
+        }
+    )
+    
+    # Verifica se a resposta é válida
+    if(is.null(resposta) || resposta$status_code != 200) {
+        warning("Falha na resposta da API!")
+        return(NULL)
+    } 
+    
+    # Parse do JSON (carga útil)
+    dados <- fromJSON(content(resposta, as="text", encoding="UTF-8"))
+
+    # Retorna os dados estruturados
+    df <- as.data.frame(dados$items)
+    return(df)
 }
 
 
